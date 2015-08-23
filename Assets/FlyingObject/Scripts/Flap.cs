f37170aa131f093a3bf508e528181da26f4e4df2
SUGINO Hironori <komenari@gmail.com>
2015-08-23 15:27:41 +0900

    position.yの値を重力で落ちながら、抵抗する逆の力を一定の間隔で加えることで、羽ばたきの動きを表現 add new scene "Flappy.unity"

---
+++ Assets/FlyingObject/Scripts/Flap.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flap : MonoBehaviour
+{
+    public float gravity = 9.8f;
+    public float flapInterval = 1.0f;
+    float upVelocity = 0;
+    Flying flying;
+
+    void Start()
+    {
+        Invoke("ShowUpVel", 1f);
+        InvokeRepeating("FlapUpdate", 0, flapInterval);
+        flying = GetComponent<Flying>();
+    }
+    void ShowUpVel()
+    {
+        Debug.Log(upVelocity);
+    }
+    void Update()
+    {
+        upVelocity -= gravity * Time.deltaTime;
+        transform.position += Vector3.up * upVelocity;
+    }
+    void FlapUpdate()
+    {
+        upVelocity += -upVelocity + 0.5f * gravity * flapInterval
+        * (transform.position.y < flying.target.position.y ? 1.5f : 0.5f);
+    }
+}
--- Assets/FlyingObject/Scripts/Flying.cs
+++ Assets/FlyingObject/Scripts/Flying.cs
@@ -7,6 +7,7 @@ public class Flying : MonoBehaviour
     public TargetObject target;
     public float speed = 1.0f;
     public float maxDeltaRotate = 45f;
+    public bool ignoreY;
 
     // Use this for initialization
     void Start()
@@ -18,10 +19,15 @@ public class Flying : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 to = target.position - transform.position;
+        Vector3 targetPos = target.position;
+        Vector3 position = transform.position;
+        if (ignoreY)
+            targetPos.y = position.y;
+        Vector3 to = targetPos - position;
         Quaternion toRot = Quaternion.LookRotation(to, Vector3.up);
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, toRot, maxDeltaRotate * Time.deltaTime);
         transform.position += transform.forward * speed * Time.deltaTime;
+		
     }
 }
